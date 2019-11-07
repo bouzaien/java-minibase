9b36d9173e0e6795046ea4db0c3af249779baddc
bouzaien <mokhles.bouzaien@eurecom.fr>
2019-11-08 00:20:46 +0100

    Add FIFO methods

--- src/bufmgr/FIFO.java
+++ src/bufmgr/FIFO.java
@@ -2,6 +2,42 @@
 
 package bufmgr;
 
-public class FIFO {
+public class FIFO extends Replacer {
+	
+	private int  frames[];
+	private int  nframes;
+	
+    public FIFO(BufMgr mgrArg)
+    {
+    	super(mgrArg);
+    	frames = null;
+    }
+	
+	private void update(int frameNo)
+	//TODO
+	{}
+	
+	public void setBufferManager( BufMgr mgr )
+    {
+		super.setBufferManager(mgr);
+		frames = new int [ mgr.getNumBuffers() ];
+		nframes = 0;
+    }
+	
+	public void pin(int frameNo) throws InvalidFrameNumberException
+	{
+		super.pin(frameNo);
+		update(frameNo);
+	}
+	
+	public int pick_victim()
+	//TODO
+	{
+		return -1;
+	}
 
+	public String name() {
+		return "FIFO";
+	}
+	
 }
