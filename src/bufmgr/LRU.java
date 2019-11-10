76545ecc3b7192ce74c3025d71134b8051c7fe85
bouzaien <mokhles.bouzaien@eurecom.fr>
2019-11-10 13:31:46 +0100

    Fix indentation

--- src/bufmgr/LRU.java
+++ src/bufmgr/LRU.java
@@ -15,30 +15,29 @@ class LRU extends  Replacer {
    * private field
    * An array to hold number of frames in the buffer pool
    */
-
-    private int  frames[];
+	private int  frames[];
  
   /**
    * private field
    * number of frames used
-   */   
-  private int  nframes;
+   */
+	private int  nframes;
 
   /**
    * This pushes the given frame to the end of the list.
    * @param frameNo	the frame number
    */
-  private void update(int frameNo)
-  {
-     int index;
-     for ( index=0; index < nframes; ++index )
-        if ( frames[index] == frameNo )
-            break;
-
-    while ( ++index < nframes )
-        frames[index-1] = frames[index];
-        frames[nframes-1] = frameNo;
-  }
+	private void update(int frameNo)
+	{
+		int index;
+		for ( index=0; index < nframes; ++index )
+			if ( frames[index] == frameNo )
+				break;
+		
+		while ( ++index < nframes )
+			frames[index-1] = frames[index];
+		frames[nframes-1] = frameNo;
+	}
 
   /**
    * Calling super class the same method
@@ -50,12 +49,12 @@ class LRU extends  Replacer {
    * @see	BufMgr
    * @see	Replacer
    */
-    public void setBufferManager( BufMgr mgr )
-     {
-        super.setBufferManager(mgr);
-	frames = new int [ mgr.getNumBuffers() ];
-	nframes = 0;
-     }
+	public void setBufferManager( BufMgr mgr )
+	{
+		super.setBufferManager(mgr);
+		frames = new int [ mgr.getNumBuffers() ];
+		nframes = 0;
+	}
 
 /* public methods */
 
@@ -63,11 +62,11 @@ class LRU extends  Replacer {
    * Class constructor
    * Initializing frames[] pinter = null.
    */
-    public LRU(BufMgr mgrArg)
-    {
-      super(mgrArg);
-      frames = null;
-    }
+	public LRU(BufMgr mgrArg)
+	{
+		super(mgrArg);
+		frames = null;
+	}
   
   /**
    * calll super class the same method
@@ -77,13 +76,11 @@ class LRU extends  Replacer {
    * @param	 frameNo	 the frame number to pin
    * @exception  InvalidFrameNumberException
    */
- public void pin(int frameNo) throws InvalidFrameNumberException
- {
-    super.pin(frameNo);
-
-    update(frameNo);
-    
- }
+	public void pin(int frameNo) throws InvalidFrameNumberException
+	{
+		super.pin(frameNo);
+		update(frameNo);
+	}
 
   /**
    * Finding a free frame in the buffer pool
@@ -92,59 +89,55 @@ class LRU extends  Replacer {
    * @return 	return the frame number
    *		return -1 if failed
    */
-
- public int pick_victim()
- {
-   int numBuffers = mgr.getNumBuffers();
-   int frame;
-   
-    if ( nframes < numBuffers ) {
-        frame = nframes++;
-        frames[frame] = frame;
-        state_bit[frame].state = Pinned;
-        (mgr.frameTable())[frame].pin();
-        return frame;
-    }
-
-    for ( int i = 0; i < numBuffers; ++i ) {
-         frame = frames[i];
-        if ( state_bit[frame].state != Pinned ) {
-            state_bit[frame].state = Pinned;
-            (mgr.frameTable())[frame].pin();
-            update(frame);
-            return frame;
-        }
-    }
-
-    return -1;
- }
+	public int pick_victim()
+	{
+		int numBuffers = mgr.getNumBuffers();
+		int frame;
+		
+		if ( nframes < numBuffers ) {
+			frame = nframes++;
+			frames[frame] = frame;
+			state_bit[frame].state = Pinned;
+			(mgr.frameTable())[frame].pin();
+			return frame;
+		}
+		
+		for ( int i = 0; i < numBuffers; ++i ) {
+			frame = frames[i];
+			if ( state_bit[frame].state != Pinned ) {
+				state_bit[frame].state = Pinned;
+				(mgr.frameTable())[frame].pin();
+				update(frame);
+				return frame;
+			}
+		}
+		
+		return -1;
+	}
  
   /**
    * get the page replacement policy name
    *
    * @return	return the name of replacement policy used
    */  
-    public String name() { return "LRU"; }
+	public String name() { return "LRU"; }
  
   /**
    * print out the information of frame usage
    */  
- public void info()
- {
-    super.info();
-
-    System.out.print( "LRU REPLACEMENT");
-    
-    for (int i = 0; i < nframes; i++) {
-        if (i % 5 == 0)
-	System.out.println( );
-	System.out.print( "\t" + frames[i]);
-        
-    }
-    System.out.println();
- }
-  
-}
+    public void info()
+    {
+    	super.info();
+    	System.out.print( "LRU REPLACEMENT");
+    	
+    	for (int i = 0; i < nframes; i++) {
+    		if (i % 5 == 0)
+    			System.out.println( );
+    		System.out.print( "\t" + frames[i]);
+    	}
+    	System.out.println();
+    } // End info()
+} // End LRU()
 
 
 
