b60875d92de48ef607c00f51486b1e510aadb7f0
Laila Salhi <salhi@cenis.eurecom.fr>
2019-11-12 16:14:52 +0100

    setBufferManager, update and pick_victim modified for LIFO based on MRU

--- src/bufmgr/LIFO.java
+++ src/bufmgr/LIFO.java
@@ -12,27 +12,73 @@ public class LIFO extends Replacer {
     	super(mgrArg);
     	frames = null;
     }
-	
-	private void update(int frameNo)
-	//TODO
-	{}
-	
-	public void setBufferManager( BufMgr mgr )
+    
+    public void setBufferManager( BufMgr mgr )
     {
 		super.setBufferManager(mgr);
-		frames = new int [ mgr.getNumBuffers() ];
-		nframes = 0;
+
+		int numBuffers = mgr.getNumBuffers();
+		frames = new int[numBuffers];
+
+		for ( int index = 0; index < numBuffers; ++index )
+			frames[index] = -index;
+
+		frames[0] = -numBuffers;
     }
+	
+	private void update(int frameNo)
+	{
+		int index;
+		int numBuffers=mgr.getNumBuffers();
+		for ( index=0; index < numBuffers; ++index )
+			if ( frames[index] < 0  ||  frames[index] == frameNo )
+				break;
+
+		// If buffer pool is not yet full, add this frame to it...
+		if ( frames[index] < 0 )
+			frames[index] = frameNo;
+
+		int frame = frames[index];
+		while ( index-- > 0 )
+			frames[index+1] = frames[index];
+
+		frames[0] = frame;
+	}
 	
 	public void pin(int frameNo) throws InvalidFrameNumberException
 	{
 		super.pin(frameNo);
-		update(frameNo);
 	}
 
-	public int pick_victim() throws BufferPoolExceededException, PagePinnedException {
-		// TODO
-		return 0;
+	public int pick_victim(){
+
+		int numBuffers = mgr.getNumBuffers();
+		int i, frame;
+
+		for ( i = 0; i < numBuffers; ++i )
+			if (frames[i] < 0) {
+				if ( i == 0 )
+					frames[i] = 0;
+				else
+					frames[i] *= -1;
+				frame = frames[i];
+				state_bit[frame].state = Pinned;
+				(mgr.frameTable())[frame].pin();
+				update(frame);
+				return frame;
+			}
+
+		for ( i = 0; i < numBuffers; ++i ) {
+			frame = frames[i];
+			if ( state_bit[frame].state != Pinned ) {
+				state_bit[frame].state = Pinned;
+				(mgr.frameTable())[frame].pin();
+				update(frame);
+				return frame;
+			}
+		}
+
+		return -1;   // No victims found!!
 	}
 
 	public String name() {
