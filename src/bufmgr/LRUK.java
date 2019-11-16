b1e188fac39369e0e8acd7c9a06fbe5959ef18a9
bouzaien <mokhles.bouzaien@eurecom.fr>
2019-11-16 20:32:08 +0100

    Implement update for LRUK

--- src/bufmgr/LRUK.java
+++ src/bufmgr/LRUK.java
@@ -2,6 +2,13 @@ package bufmgr;
 
 public class LRUK extends Replacer {
 
+	private static final long CRP = 0;
+
+	/**
+	 * number of last references to consider
+	 */
+	private int lastRef;
+
 	/**
 	 * private field
 	 * An array to hold number of frames in the buffer pool
@@ -15,12 +22,72 @@ public class LRUK extends Replacer {
 	private int nframes;
 
 	/**
+	 * Denotes the time of last i references of page frameNo
+	 * hist[frameNo][i]
+	 */
+	private long hist[][];
+
+	/**
+	 * The time of the most recent reference to page frameNo
+	 * last[frameNo]
+	 */
+	private long last[];
+
+
+	/**
 	 * This pushes the given frame to the end of the list.
 	 * @param frameNo	the frame number
 	 */
 	private void update(int frameNo)
-	{
+	{	
+		long 	t = System.currentTimeMillis();
+		int 	index;
+		boolean	exist = false; // To check if the page is already in the buffer
+		for ( index=0; index < nframes; ++index )
+			if ( frames[index] == frameNo ) {
+				exist = true;
+				break;
+			}
 
+		if (exist) {
+			if ( t-last[frameNo]>= CRP ) {
+				long refrencedPageCP = last[frameNo] - hist[frameNo][1];
+				for (int i=2; i<=lastRef; i++) {
+					hist[frameNo][i] = hist[frameNo][i-1] + refrencedPageCP;
+				}
+				hist[frameNo][1] = t;
+				last[frameNo] = t;
+			} else {
+				last[frameNo] = t;
+			}
+		} else {
+			long min = t;
+			for ( int i=0; i<nframes; i++) {
+				int frame = frames[i];
+				if ( t-last[frame]>= CRP && hist[frame][lastRef]<=min) {
+					int victim = frame;
+					min = hist[frame][lastRef];
+				}
+			}
+
+			if (true /*victim is dirty*/) {
+				/*write victim to database*/
+			}
+
+			// TODO
+			//fetch frameNo into the victim frame
+
+			if (true /* hist[frameNo] does not exist */) {
+				// allocate hist[frameNo]??
+				for (int i=2; i<=lastRef; i++)
+					hist[frameNo][i] = (long) 0;
+			} else {
+				for (int i=2; i<=lastRef; i++)
+					hist[frameNo][i] = hist[frameNo][i-1];
+			}
+			hist[frameNo][1] = t;
+			last[frameNo] = t;
+		}
 	}
 
 	/**
@@ -38,6 +105,7 @@ public class LRUK extends Replacer {
 		super.setBufferManager(mgr);
 		frames = new int [ mgr.getNumBuffers() ];
 		nframes = 0;
+		lastRef = mgr.getLastRef();
 	}
 
 	/* public methods */
@@ -50,6 +118,7 @@ public class LRUK extends Replacer {
 	{
 		super(mgrArg);
 		frames = null;
+		lastRef = mgrArg.getLastRef();
 	}
 
 	/**
@@ -101,4 +170,4 @@ public class LRUK extends Replacer {
 		System.out.println();
 	} // End info()
 
-}// End LRU_k
+}// End LRUK
